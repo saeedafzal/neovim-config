1d891abbd3416dd2067743e95827e834d10db221
Saeed Afzal <saeed.afzal@protonmail.com>
2023-02-06 22:20:45 +0000

    Update tokyonight theme configuration

--- lua/plugins/config/tokyonight.lua
+++ lua/plugins/config/tokyonight.lua
@@ -1,6 +1,19 @@
 require("tokyonight").setup {
     style = "night",
+    terminal_colors = true,
+    styles = {
+        conditionals = { italic = true, bold = true },
+        loops = { italic = true, bold = true },
+        functions = { italic = true, bold = true },
+        keywords = { italic = true, bold = true },
+        strings = { bold = true },
+        variables = { bold = true },
+        numbers = { bold = true },
+        booleans = { bold = true },
+        properties = { italic = true },
+        types = { italic = true, bold = true }
+    },
     lualine_bold = true
 }
 
--- vim.cmd [[colorscheme tokyonight]]
+vim.cmd [[colorscheme tokyonight]]
