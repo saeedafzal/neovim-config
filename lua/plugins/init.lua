263a774e316a74552a60ab3560cab32df5e4b54a
Saeed Afzal <saeed.afzal@protonmail.com>
2024-08-02 09:12:49 +0100

    Add treesitter and go plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -8,5 +8,11 @@ return {
     {
         "j-morano/buffer_manager.nvim",
         requires = "nvim-lua/plenary.nvim"
+    },
+    {
+        "ray-x/lsp_signature.nvim",
+        event = "VeryLazy",
+        opts = {},
+        config = function(_, opts) require "lsp_signature".setup(opts) end
     }
 }
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,13 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+        local configs = require("nvim-treesitter.configs")
+
+        configs.setup({
+            ensure_installed = { "c", "lua", "vimdoc", "go", "typescript" },
+            highlight = { enable = true },
+            indent = { enable = true }
+        })
+    end
+}
