871d8ebc66a7f878b3b92c47f77a83da2fd138f6
Saeed Afzal <saeed.afzal@protonmail.com>
2025-09-11 16:15:54 +0100

    Add plugins and support light theme.

--- init.lua
+++ init.lua
@@ -9,6 +9,7 @@ vim.o.clipboard = "unnamedplus"
 vim.o.signcolumn = "yes"
 vim.o.splitbelow = true
 vim.o.splitright = true
+vim.o.termguicolors = true
 
 -- Indentation
 vim.o.expandtab = true
@@ -24,14 +25,24 @@ vim.o.smartcase = true
 vim.o.hlsearch = true
 vim.o.incsearch = true
 
+vim.o.wildignore = "**/node_modules/**"
+vim.o.path = ".,**"
+
 -- Keybindings
 vim.keymap.set("n", "<leader>o", ":update<CR>:source<CR>")
 vim.keymap.set("n", "<Tab>", ":bnext<CR>")
 vim.keymap.set("n", "<S-Tab>", ":bprevious<CR>")
 vim.keymap.set("n", "<leader>x", ":bd!<CR>")
 vim.keymap.set("n", "<leader>h", ":noh<CR>")
-
 vim.keymap.set("n", "<C-n>", ":Exp<CR>")
+
+vim.keymap.set("n", "<leader>ff", function() Snacks.picker.files({ cmd = "fd" }) end)
+vim.keymap.set("n", "<leader>fw", function() Snacks.picker.grep() end)
+
+vim.keymap.set("n", "<leader>.", function() Snacks.scratch() end)
+vim.keymap.set("n", "<leader>S", function() Snacks.scratch.select() end)
+
+vim.keymap.set("n", "<leader>gb", function() Snacks.git.blame_line() end)
 
 -- Commands
 local function indent(n, expand)
@@ -56,6 +67,17 @@ vim.api.nvim_create_user_command("Spaces", function(v)
     indent(v.args, true)
 end, { nargs = "?" })
 
+vim.api.nvim_create_autocmd("ColorScheme", {
+    pattern = "*",
+    callback = function()
+        vim.cmd [[
+            hi Normal guibg=none ctermbg=none
+            hi NonText guibg=none ctermbg=none
+            hi NormalFloat guibg=none ctermbg=none
+        ]]
+    end
+})
+
 vim.api.nvim_create_user_command("TE", function(v)
     vim.cmd("split")
     vim.cmd("term " .. v.args)
@@ -79,18 +101,22 @@ vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
 
 -- Plugins
 vim.pack.add({
-    { src = "https://github.com/wakatime/vim-wakatime" }
+    { src = "https://github.com/wakatime/vim-wakatime" },
+    { src = "https://github.com/folke/snacks.nvim" }
 })
 
--- Setup plugins
+-- Snacks
+require("snacks").setup {
+    git = {
+        enabled = true,
+        border = "rounded"
+    },
+    picker = {
+        enabled = true,
+        ui_select = true
+    },
+    scratch = { enabled = true }
+}
 
 -- Theme
-vim.cmd [[
-    hi Normal guibg=none
-    hi NonText guibg=none
-    hi Normal ctermbg=none
-    hi NonText ctermbg=none
-
-    hi statusline guibg=none
-    hi statusline guifg=white
-]]
+vim.cmd [[colorscheme default]]
